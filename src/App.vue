cfa510b7fa3c86f664689648ce606d60e4af0303
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-06 13:13:52 +0800

    add basic layouts.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,52 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import { RiGithubLine, RiLinkedinLine, RiInstagramLine } from '@remixicon/vue'
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <AppHeader />
+  </header>
+  <main class="flex justify-between my-10">
+    <div class="w-[10%] hidden md:block text-center">
+      <ul class="flex flex-col items-center absolute bottom-0 left-8">
+        <li class="py-3">
+          <a href="https://github.com/bchiang7" aria-label="GitHub" target="_blank" rel="noreferrer">
+            <RiGithubLine size="25px" class="hover:scale-125 hover:fill-[#64ffda]" />
+          </a>
+        </li>
+        <li class="py-3">
+          <a href="https://www.instagram.com/bchiang7" aria-label="Instagram" target="_blank" rel="noreferrer">
+            <RiInstagramLine size="25px" class="hover:scale-125 hover:fill-[#64ffda]" />
+          </a>
+        </li>
+        <li class="py-3">
+          <a href="https://www.linkedin.com/in/bchiang7" aria-label="Linkedin" target="_blank" rel="noreferrer">
+            <RiLinkedinLine size="25px" class="hover:scale-125 hover:fill-[#64ffda]" />
+          </a>
+        </li>
+        <li class="pt-3">
+          <div class="h-28 border-r border-gray-400"></div>
+        </li>
+      </ul>
+    </div>
+    <div class="w-full">
+      <RouterView />
+    </div>
+    <div class="w-[10%] hidden md:block text-center">
+      <ul class="flex flex-col items-center absolute bottom-0 right-8">
+        <li class="py-3">
+          <a class="font-[SFMonoRegular] text-[12px] vertical-rl hover:text-[#64ffda] hover:scale-105"
+            href="mailto:virgiliocalimlim@gmail.com">virgiliocalimlim@gmail.com</a>
+        </li>
+        <li class="pt-3">
+          <div class="h-28 border-r border-gray-400"></div>
+        </li>
+      </ul>
+    </div>
+  </main>
+  <footer>
+
+  </footer>
 </template>
