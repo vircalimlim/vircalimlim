59074a55ac55e3d9cfa38b148cf43c506e0f55dd
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-07 10:42:19 +0800

    Create HeroSection.vue

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <div class="pb-6">
+            <p class="font-[SFMonoRegular] text-[#eb4432]">Hi, my name is</p>
+        </div>
+        <div class="pb-2">
+            <h1 class="text-6xl font-extrabold text-[#ccd6f6]">Virgilio Calimlim.</h1>
+        </div>
+        <div class="pb-6">
+            <h1 class="text-6xl text font-extrabold text-[#8892b0]">Artisan of web development.</h1>
+        </div>
+        <div>
+            <p class="w-full sm:w-4/5 md:w-3/5">
+                I’m a software engineer specializing in building (and occasionally designing) exceptional digital
+                experiences. Currently, I’m focused on building accessible, human-centered products
+            </p>
+        </div>
+        <div class="mt-10">
+            <button class="text-[#eb4432] border border-[#eb4432] rounded cursor-pointer px-10 py-3">Contact me</button>
+        </div>
+    </div>
+</template>
