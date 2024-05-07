8ee2e5d818421eae9809e5025040258cdd0e4094
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-07 10:42:45 +0800

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HeroSection from '../components/HeroSection.vue'
+import AboutSection from '../components/AboutSection.vue'
+</script>
+
+<template>
+  <section id="hero">
+    <HeroSection />
+  </section>
+  <section class="mt-20" id="about">
+    <AboutSection />
+  </section>
+</template>
