c2cfad41b733c5921331d6d8c4aba58141aa5c7c
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-09 17:56:13 +0800

    Create ProjectSection.vue

---
+++ src/components/ProjectSection.vue
@@ -0,0 +1,171 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h2 class="header-title text-[#ccd6f6] text-[32px] font-[600]">Projects</h2>
+    </div>
+
+    <div class="mt-5">
+
+        <div
+            class="flex flex-wrap-reverse sm:flex-nowrap items-start space-y-4 sm:space-y-0 space-y-reverse sm:space-x-5 md:space-x-8 p-4 mb-2 rounded hover:bg-slate-700">
+            <div class="w-lg">
+                <img class="w-48 h-24 sm:h-20 object-cover rounded shadow-xl cursor-pointer"
+                    src="https://blog.masterofproject.com/wp-content/uploads/2019/10/project-plan-example-2.jpg"
+                    alt="project-image">
+            </div>
+            <div class="w-full flex flex-col space-y-2">
+                <h3 class="text-lg text-slate-200">Project Name</h3>
+                <p class="text-justify text-[18px]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laborum minus aut voluptatibus
+                    magni ipsam aspernatur. Modi deserunt eum soluta reiciendis qui quibusdam culpa sed laudantium,
+                    assumenda voluptas praesentium consequatur!
+                </p>
+                <ul class="flex flex-wrap" aria-label="Technologies used">
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Laravel</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Php</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Mysql</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Vue</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Firebase</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div
+            class="flex flex-wrap-reverse sm:flex-nowrap items-start space-y-4 sm:space-y-0 space-y-reverse sm:space-x-5 md:space-x-8 p-4 mb-2 rounded hover:bg-slate-700">
+            <div class="w-lg">
+                <img class="w-48 h-24 sm:h-20 object-cover rounded shadow-xl cursor-pointer"
+                    src="https://blog.masterofproject.com/wp-content/uploads/2019/10/project-plan-example-2.jpg"
+                    alt="project-image">
+            </div>
+            <div class="w-full flex flex-col space-y-2">
+                <h3 class="text-lg text-slate-200">Project Name</h3>
+                <p class="text-justify text-[18px]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laborum minus aut voluptatibus
+                    magni ipsam aspernatur. Modi deserunt eum soluta reiciendis qui quibusdam culpa sed laudantium,
+                    assumenda voluptas praesentium consequatur!
+                </p>
+                <ul class="flex flex-wrap" aria-label="Technologies used">
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Laravel</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Php</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Mysql</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Vue</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Firebase</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div
+            class="flex flex-wrap-reverse sm:flex-nowrap items-start space-y-4 sm:space-y-0 space-y-reverse sm:space-x-5 md:space-x-8 p-4 mb-2 rounded hover:bg-slate-700">
+            <div class="w-lg">
+                <img class="w-48 h-24 sm:h-20 object-cover rounded shadow-xl cursor-pointer"
+                    src="https://blog.masterofproject.com/wp-content/uploads/2019/10/project-plan-example-2.jpg"
+                    alt="project-image">
+            </div>
+            <div class="w-full flex flex-col space-y-2">
+                <h3 class="text-lg text-slate-200">Project Name</h3>
+                <p class="text-justify text-[18px]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laborum minus aut voluptatibus
+                    magni ipsam aspernatur. Modi deserunt eum soluta reiciendis qui quibusdam culpa sed laudantium,
+                    assumenda voluptas praesentium consequatur!
+                </p>
+                <ul class="flex flex-wrap" aria-label="Technologies used">
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Laravel</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Php</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Mysql</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Vue</div>
+                    </li>
+                    <li class="mr-1.5 mt-2">
+                        <div
+                            class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                            Firebase</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.header-title {
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    width: 100%;
+}
+
+.header-title::after {
+    display: inline-block;
+    content: "";
+    border-top: 1px solid #8892b0;
+    width: 20rem;
+    margin: 0 1rem;
+    /* transform: translateY(-10px); */
+}
+
+@media (max-width: 767px) {
+
+    .header-title::after {
+        width: 70%;
+        text-align: end;
+    }
+
+}
+</style>
