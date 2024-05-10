192f2931a7028851a75e807922a382a734c319b5
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-10 10:52:22 +0800

    add tech button componebt.

---
+++ src/components/TechButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const { techs } = defineProps({
+    techs: { type: Array<string>, required: true }
+})
+
+</script>
+
+<template>
+    <ul class="flex flex-wrap" aria-label="Technologies used">
+        <li v-for="tech in techs" class="mr-1.5 mt-2">
+            <div
+                class="flex items-center rounded-full bg-red-500 px-3 py-1 text-xs font-bold leading-5 text-white hover:bg-red-400">
+                {{ tech }}</div>
+        </li>
+    </ul>
+</template>
