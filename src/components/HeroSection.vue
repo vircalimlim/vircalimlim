b13459a16fe4ba39b95902d6b87f1986635186a3
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-13 17:52:14 +0800

    apply animation to hero section.

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
+import { gsap } from "gsap";
+import BounceTransition from './BounceTransition.vue'
+import FadeInTransition from './FadeInTransition.vue'
 
 onMounted(() => {
     var button = document.getElementById('shakeButton')!;
@@ -36,24 +39,30 @@ onMounted(() => {
             <h1 class="text-4xl sm:text-5xl md:text-6xl font-extrabold text-[#ccd6f6]">Virgilio Calimlim.</h1>
         </div>
         <div class="pb-5">
-            <h1 class="text-4xl sm:text-5xl md:text-6xl text font-extrabold text-[#8892b0]">Artisan of web development.
+            <h1 class="text-4xl sm:text-5xl md:text-6xl text font-extrabold text-[#8892b0]">Artisan of web
+                development.
             </h1>
         </div>
-        <div>
-            <p class="max-w-lg text-justify">
-                I'm software developer who is passionate about creating user-friendly applications from concept to
-                deployment. With problem-solving expertise and a commitment to staying current with industry trends, I
-                thrive in collaborative environments that foster creativity. Let's work together to create something
-                amazing!
-            </p>
-        </div>
-        <div class="mt-5">
-            <span class="button-pop-wrapper py-3">
-                <button id="shakeButton" class="contact-button button-pop px-10 py-3">
-                    Let's talk 👋
-                </button>
-            </span>
-        </div>
+        <FadeInTransition>
+            <div>
+                <p class="max-w-lg text-justify">
+                    I'm software developer who is passionate about creating user-friendly applications from concept to
+                    deployment. With problem-solving expertise and a commitment to staying current with industry trends,
+                    I
+                    thrive in collaborative environments that foster creativity. Let's work together to create something
+                    amazing!
+                </p>
+            </div>
+        </FadeInTransition>
+        <BounceTransition>
+            <div class="mt-5">
+                <span class="button-pop-wrapper py-3">
+                    <button id="shakeButton" class="contact-button button-pop px-10 py-3">
+                        Let's talk 👋
+                    </button>
+                </span>
+            </div>
+        </BounceTransition>
     </div>
 </template>
 
