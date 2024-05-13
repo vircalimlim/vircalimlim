14d57fa5375ce7e00365f79024b66d9e460b0f90
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-13 15:14:48 +0800

    Create ContactSection.vue

---
+++ src/components/ContactSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex flex-col space-y-3 justify-center">
+        <span class="text-center text-[#eb4432]">Got an offer for me?</span>
+        <h1 class="text-4xl sm:text-4xl md:text-5xl font-bold text-center text-slate-200">Get in touch</h1>
+        <div class="text-center">
+            <span class="button-pop-wrapper py-3">
+                <a href="mailto:virgiliocalimlim@gmail.com" id="shakeButton"
+                    class="contact-button button-pop px-10 py-3">
+                    Send email
+                </a>
+            </span>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
