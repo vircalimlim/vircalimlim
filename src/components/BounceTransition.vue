59e5f0dc040032f5a80820089686892acbf67952
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-13 17:51:57 +0800

    reusable animation components.

---
+++ src/components/BounceTransition.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { gsap } from "gsap";
+
+const onBeforeEnter = (el: Object) => {
+    el.style.opacity = 0;
+    el.style.transform = 'translateY(-100px)'
+}
+
+const enter = (el: Object, done) => {
+    gsap.to(el, {
+        duration: 2.5,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+    })
+}
+</script>
+
+<template>
+    <Transition appear @before-enter="onBeforeEnter" @enter="enter">
+        <slot></slot>
+    </Transition>
+</template>
---
+++ src/components/FadeInTransition.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { gsap } from "gsap";
+
+const onBeforeEnter = (el: Object) => {
+    el.style.opacity = 0;
+    el.style.transform = 'translateX(-200px)'
+}
+
+const enter = (el: Object, done) => {
+    gsap.to(el, {
+        duration: .8,
+        opacity: 1,
+        x: 0,
+        onComplete: done
+    })
+}
+</script>
+
+<template>
+    <Transition appear @before-enter="onBeforeEnter" @enter="enter">
+        <slot></slot>
+    </Transition>
+</template>
