1bd3633a43f5340ec1492f7e5a0998224b7b6281
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-14 17:00:21 +0800

    try type any.

--- src/components/FadeInTransition.vue
+++ src/components/FadeInTransition.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { gsap } from "gsap";
 
-const onBeforeEnter = (el: HTMLElement) => {
+const onBeforeEnter: any = (el: HTMLElement) => {
     el.style.opacity = '0';
     el.style.transform = 'translateX(-200px)'
 }
 
 // https://stackoverflow.com/a/29704013 = typescript type callback function
-const enter = (el: HTMLElement, done: () => void) => {
+const enter: any = (el: HTMLElement, done: () => void) => {
     gsap.to(el, {
         duration: .8,
         opacity: 1,
