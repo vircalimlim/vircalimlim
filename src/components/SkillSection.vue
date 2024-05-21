d28bb70249847383b9b5fc62b5fcd4e1d8a8e764
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-05-21 14:16:49 +0800

    Update SkillSection.vue

--- src/components/SkillSection.vue
+++ src/components/SkillSection.vue
@@ -2,16 +2,23 @@
 import TechCard from './TechCard.vue';
 
 const main_techs = [
-    { title: 'PHP', url: 'php.svg' },
     { title: 'LARAVEL', url: 'laravel.svg' },
+    { title: 'PHP', url: 'php.svg' },
     { title: 'MYSQL', url: 'mysql.svg' },
     { title: 'VUEJS', url: 'vuejs.svg' },
     { title: 'NUXTJS', url: 'nuxtjs.svg' },
+    { title: 'reactjs', url: 'reactjs.svg' },
+    { title: 'nextjs', url: 'nextjs.svg' },
+    { title: 'jquery', url: 'jquery.svg' },
     { title: 'NODEJS', url: 'nodejs.svg' },
     { title: 'typescript', url: 'typescript.svg' },
     { title: 'firebase', url: 'firebase.svg' },
     { title: 'tailwind', url: 'tailwind.svg' },
     { title: 'bootstrap', url: 'bootstrap5.svg' },
+    { title: 'chakraui', url: 'chakraui.svg' },
+    { title: 'daisyui', url: 'daisyui.svg' },
+    { title: 'flutter', url: 'flutter.svg' },
+    { title: 'expo', url: 'react-expo.svg' },
     { title: 'html', url: 'html.svg' },
     { title: 'css', url: 'css.svg' },
     { title: 'javascript', url: 'javascript.svg' },
@@ -35,17 +42,17 @@ const other_techs = [
 
     <div class="mt-9 pl-5">
         <div class="">
-            <h1 class="skill-category text-lg text-slate-500">Main</h1>
-            <div class="flex flex-wrap justify-start">
+            <!-- <h1 class="skill-category text-lg text-slate-500">Main</h1> -->
+            <div class="max-w-lg flex flex-wrap justify-start">
                 <TechCard :techs="main_techs" />
             </div>
         </div>
-        <div class="mt-5">
+        <!-- <div class="mt-5">
             <h1 class="skill-category text-lg text-slate-500">Other</h1>
             <div class="flex flex-wrap justify-start">
                 <TechCard :techs="other_techs" />
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
