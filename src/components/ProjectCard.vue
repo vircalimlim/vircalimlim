193e6959be5602d27762601fcfbb8840fd713ca8
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-07-25 17:19:59 +0800

    update project section with real data.

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import TechButton from './TechButton.vue';
+
+const { techs } = defineProps({
+    url: {
+        type: String,
+        required: true,
+    },
+    imgUrl: {
+        type: String,
+        required: true,
+    },
+    project_name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    techs: {
+        type: Array<string>,
+        required: true,
+    }
+})
+
+const redirectToUrl = (url: string) => {
+    window.open(url, '_blank')
+}
+</script>
+
+<template>
+    <div @click="redirectToUrl(url)"
+        class="flex flex-wrap-reverse sm:flex-nowrap items-start space-y-4 sm:space-y-0 space-y-reverse sm:space-x-5 md:space-x-8 p-4 mb-2 rounded cursor-pointer hover:bg-slate-700">
+        <div class="w-lg">
+            <img class="w-48 h-24 sm:h-20 object-cover rounded shadow-xl cursor-pointer" :src="imgUrl"
+                :alt="project_name">
+        </div>
+        <div class="w-full flex flex-col space-y-2">
+            <h3 class="text-lg text-slate-200">{{ project_name }}</h3>
+            <p class="text-justify text-[18px]">
+                {{ description }}
+            </p>
+            <TechButton :techs="techs" />
+        </div>
+    </div>
+</template>
--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ProjectCard from './ProjectCard.vue';
 import TechButton from './TechButton.vue';
 
 </script>
@@ -10,41 +11,13 @@ import TechButton from './TechButton.vue';
 
     <div class="mt-5">
 
-        <div
-            class="flex flex-wrap-reverse sm:flex-nowrap items-start space-y-4 sm:space-y-0 space-y-reverse sm:space-x-5 md:space-x-8 p-4 mb-2 rounded hover:bg-slate-700">
-            <div class="w-lg">
-                <img class="w-48 h-24 sm:h-20 object-cover rounded shadow-xl cursor-pointer"
-                    src="https://blog.masterofproject.com/wp-content/uploads/2019/10/project-plan-example-2.jpg"
-                    alt="project-image">
-            </div>
-            <div class="w-full flex flex-col space-y-2">
-                <h3 class="text-lg text-slate-200">Project Name</h3>
-                <p class="text-justify text-[18px]">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laborum minus aut voluptatibus
-                    magni ipsam aspernatur. Modi deserunt eum soluta reiciendis qui quibusdam culpa sed laudantium,
-                    assumenda voluptas praesentium consequatur!
-                </p>
-                <TechButton :techs="['Laravel', 'Php', 'Mysql', 'Vue', 'Firebase']" />
-            </div>
-        </div>
+        <ProjectCard url="https://react-next-shopping.vercel.app" imgUrl="/images/tokoshop.PNG" project_name="TokoShop"
+            description="TokoShop offers a user-friendly interface for online shopping, where users can browse, select, and purchase various products. Key features include browsing products, managing orders, and user login. The design is modern and responsive, aiming to provide a seamless shopping experience."
+            :techs="['Nextjs', 'Firebase', 'Axios', 'Stripe', 'ChakraUi']" />
 
-        <div
-            class="flex flex-wrap-reverse sm:flex-nowrap items-start space-y-4 sm:space-y-0 space-y-reverse sm:space-x-5 md:space-x-8 p-4 mb-2 rounded hover:bg-slate-700">
-            <div class="w-lg">
-                <img class="w-48 h-24 sm:h-20 object-cover rounded shadow-xl cursor-pointer"
-                    src="https://blog.masterofproject.com/wp-content/uploads/2019/10/project-plan-example-2.jpg"
-                    alt="project-image">
-            </div>
-            <div class="w-full flex flex-col space-y-2">
-                <h3 class="text-lg text-slate-200">Project Name</h3>
-                <p class="text-justify text-[18px]">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laborum minus aut voluptatibus
-                    magni ipsam aspernatur. Modi deserunt eum soluta reiciendis qui quibusdam culpa sed laudantium,
-                    assumenda voluptas praesentium consequatur!
-                </p>
-                <TechButton :techs="['Laravel', 'Php', 'Mysql', 'Vue', 'Firebase']" />
-            </div>
-        </div>
+        <ProjectCard url="https://react-next-shopping.vercel.app" imgUrl="/images/movies.PNG" project_name="TMDB Cinema"
+            description="TMDB Cinema is a movie discovery platform using TMDB api, where you can explore popular, top-rated, and upcoming movies. Watch trailers for specific films, search for your favorite movies, and view detailed information about each one. The site features infinite scrolling for a seamless browsing experience, making it easy to find your next favorite film."
+            :techs="['React', 'React Router', 'Bootstrap', 'Axios', 'TMDB API']" />
 
         <div
             class="flex flex-wrap-reverse sm:flex-nowrap items-start space-y-4 sm:space-y-0 space-y-reverse sm:space-x-5 md:space-x-8 p-4 mb-2 rounded hover:bg-slate-700">
